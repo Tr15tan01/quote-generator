10ddb351c5a964a161b8437475acb99f3a660e99
titevar <59051464+titevar@users.noreply.github.com>
2020-08-04 23:45:30 +0400

    first upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing')
+const authorText = document.getElementById('author');
+const quoteText = document.getElementById('quote');
+
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
+    } catch(error) {
+        getQuote();
+        console.log('could not get it ' + error)
+    }
+}
+
+getQuote()
